629cf30b36be08b35c647f16e90b06e99531134d
DoanVanNghiep <doannghiepnd2k3@gmail.com>
2024-03-14 14:29:54 +0700

    14/03/2024

---
+++ src/main/java/fita/vnua/edu/vn/xdptpmbtlnhom09/XdptpmBtlNhom09Application.java
@@ -0,0 +1,13 @@
+package fita.vnua.edu.vn.xdptpmbtlnhom09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdptpmBtlNhom09Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XdptpmBtlNhom09Application.class, args);
+    }
+
+}
---
+++ src/test/java/fita/vnua/edu/vn/xdptpmbtlnhom09/XdptpmBtlNhom09ApplicationTests.java
@@ -0,0 +1,13 @@
+package fita.vnua.edu.vn.xdptpmbtlnhom09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdptpmBtlNhom09ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
